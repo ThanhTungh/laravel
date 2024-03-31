31163c2abe106411f6797b223317a86f7f692383
ThanhTungh <tungtottinh555@gmail.com>
2024-03-31 23:46:54 +0700

    ddd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Routing\UrlGenerator;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -18,12 +17,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(UrlGenerator $url)
+    public function boot(): void
     {
         Paginator::useBootstrapFive();
-        if (env('APP_ENV') == 'production') {
-            $url->forceScheme('https');
-        }
-
     }
 }
